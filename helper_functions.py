47875ef5070276094709e3e71090028b059b57a8
OliverAltindag <85311607+OliverAltindag@users.noreply.github.com>
2025-11-14 14:55:12 -0500

    Add image processing function for folder processing
    Added a new function to process images in a folder, applying bias, dark, and flat corrections.

--- helper_functions.py
+++ helper_functions.py
@@ -4,6 +4,7 @@ import numpy as np
 from astropy.io import fits
 import matplotlib.pyplot as plt
 from scipy.ndimage import shift as scipy_shift
+import main_functions as mf
 
 # Helper Function Definitons will all be defined here:
 
@@ -170,3 +171,19 @@ def stack_all_filters(folder_path, filter_files, all_shifts, pad_val):
         y_shifts = [s[1] for s in shifts_list]
         shifting(file_list, x_shifts, y_shifts, pad_val, save_path)
     return
+
+def process_images_in_folder(base_folder_path, filter_names, master_bias_path, master_dark_path, master_flats_folder):
+    '''
+    '''
+    for filter_name in filter_names:
+        filter_subfolder_path = os.path.join(base_folder_path, filter_name)
+        # Find all .fits files in the subfolder
+        raw_files = glob.glob(os.path.join(filter_subfolder_path, '*.fits'))
+        for image_path in raw_files:
+            base_filename = os.path.basename(image_path)
+            # Skip files that have already been processed
+            if "fdb_" in base_filename:
+                continue
+            new_filename = "fdb_" + base_filename
+            final_save_path = os.path.join(filter_subfolder_path, new_filename)
+            mf.image_processing(image_path, master_bias_path, master_dark_path, master_flats_folder, final_save_path)
