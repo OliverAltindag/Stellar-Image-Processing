eccac5aa8d11ab4fe23aa336b3db0e34bfaef0ff
OliverAltindag <85311607+OliverAltindag@users.noreply.github.com>
2025-11-14 19:55:17 -0500

    Add main execution block to run.py

---
+++ run.py
@@ -0,0 +1,6 @@
+import final_reduction as reduction
+
+if __name__ == "__main__":
+	datafolder = "/absolute/path/to/datafolder/"
+objname = input("Object name: ")
+reduction.reduction(datafolder, objname)
