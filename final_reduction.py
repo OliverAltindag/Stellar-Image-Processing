14237d87cbef107931b7821fe4d6e9a5dd09fc63
OliverAltindag <85311607+OliverAltindag@users.noreply.github.com>
2025-11-16 14:37:23 -0500

    Update final_reduction.py

--- final_reduction.py
+++ final_reduction.py
@@ -99,11 +99,11 @@ def reduction(data_folder_path, science_images_folder):
 
     pad_val = 150 
     star_coords_main = [1150, 1300, 3700, 3800]
-    bg_coords_main = [850, 1000, 3650, 3750]
+    bg_coords_main = [920, 1000, 3660, 3740]
     align_and_stack_folder(science_folder_path, star_coords_main, bg_coords_main, pad_val)
     
     star_coords_std = [2650, 2900, 2050, 2250]
-    bg_coords_std = [2800, 1925, 1990, 2060]
+    bg_coords_std = [2825, 1925, 1990, 2060]
     align_and_stack_folder(standard_folder_path, star_coords_std, bg_coords_std, pad_val)
 
     master_stack_paths = glob.glob(os.path.join(science_folder_path, "master_stack_*.fit"))
