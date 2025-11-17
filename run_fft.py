9526f0aa9587397939312a88e5d6c0a7a4a67952
OliverAltindag <85311607+OliverAltindag@users.noreply.github.com>
2025-11-17 01:26:19 -0500

    Update run_fft.py

--- run_fft.py
+++ run_fft.py
@@ -1,6 +1,6 @@
 import fft_reduction as reduction
 
 if __name__ == "__main__":
-	datafolder = r"C:\Users\olive\OneDrive\Desktop\Jackson_Oliver_Nina"
+	datafolder = r"C:\Users\olive\OneDrive\Desktop\Jackson_Oliver_Nina_fft"
 	objname = input("Object name: ")
 	reduction.reduction(datafolder, objname)
