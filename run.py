9e40e14832eaaa69d659087c9367d9d5de651584
OliverAltindag <85311607+OliverAltindag@users.noreply.github.com>
2025-11-17 12:21:12 -0500

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import centroiding_reduction as reduction
 
 if __name__ == "__main__":
-	datafolder = r"C:\Users\olive\OneDrive\Desktop\Jackson_Oliver_Nina"
+	datafolder = r"C:\Users\olive\OneDrive\Desktop\Jackson_Oliver_Nina_Centroiding"
 	objname = input("Object name: ")
 	reduction.reduction(datafolder, objname)
