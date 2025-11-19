1baaa3b4703510c3f8f601fe5fcc73cde8531fb7
OliverAltindag <85311607+OliverAltindag@users.noreply.github.com>
2025-11-19 13:22:08 -0500

    Delete run.py

--- run.py
+++
@@ -1,6 +0,0 @@
-import centroiding_reduction as reduction
-
-if __name__ == "__main__":
-	datafolder = r"C:\Users\olive\OneDrive\Desktop\Jackson_Oliver_Nina_Centroiding"
-	objname = input("Object name: ")
-	reduction.reduction(datafolder, objname)
